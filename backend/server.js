175f59398194b992350b54836f3e0e7fc05da4fa
aniket <aniketshinde@gmail.com>
2025-07-08 12:35:26 +0530

    Fix: added root route to backend

--- backend/server.js
+++ backend/server.js
@@ -23,3 +23,8 @@ app.use("/api/auth", authRoutes);
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
